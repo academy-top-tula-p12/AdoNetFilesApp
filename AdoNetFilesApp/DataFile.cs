19c40e0f868a22824ced1ba54e5ac2f21491814f
maxefimov <efimov.top.academy@gmail.com>
2024-05-14 18:59:11 +0300

    14-05-24

---
+++ AdoNetFilesApp/DataFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetFilesApp
+{
+    public class DataFile
+    {
+        public int Id { get; set; }
+        public string? Title { set; get; }
+        public string? FileName { set; get; }
+        public byte[] BinaryData { set; get; }
+
+        public DataFile(int id, string? title, string? fileName, byte[] binaryData)
+        {
+            Id = id;
+            Title = title;
+            FileName = fileName;
+            BinaryData = binaryData;
+        }
+    }
+}
--- AdoNetFilesApp/Program.cs
+++ AdoNetFilesApp/Program.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using AdoNetFilesApp;
+using Microsoft.Data.SqlClient;
 using System.Data;
-
-Dictionary<string, string> files = new();
-
-files.Add("image.jpg", "Photo of Ada Lavlase");
-files.Add("docs.pdf", "Lesson for academy");
 
 string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AcademyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
+//Dictionary<string, string> files = new();
+
+//files.Add("image.jpg", "Photo of Ada Lavlase");
+//files.Add("docs.pdf", "Lesson for academy");
 
 //string commandString = @"CREATE TABLE Files
 //                         (
@@ -17,37 +17,77 @@ string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=A
 //                            binary_data VARBINARY(MAX)
 //                         )";
 
-string commandString = @"INSERT INTO Files 
-                            (title, file_name, binary_data)
-                            VALUES
-                            (@title, @file_name, @binary_data)";
+//string commandString = @"INSERT INTO Files 
+//                            (title, file_name, binary_data)
+//                            VALUES
+//                            (@title, @file_name, @binary_data)";
 
-using (SqlConnection connection = new SqlConnection(connectionString))
+//using (SqlConnection connection = new SqlConnection(connectionString))
+//{
+//    await connection.OpenAsync();
+
+//    SqlCommand command = connection.CreateCommand();
+//    command.CommandText = commandString;
+//    command.Parameters.Add("@title", SqlDbType.NVarChar, 100);
+//    command.Parameters.Add("@file_name", SqlDbType.NVarChar, 100);
+//    command.Parameters.Add("@binary_data", SqlDbType.VarBinary);
+
+//    //command.Parameters.Add("@file_name", SqlDbType.VarBinary, 100);
+//    byte[] buffer;
+
+//    foreach(var file in files)
+//    {
+//        using(FileStream stream = new(file.Key, FileMode.Open))
+//        {
+//            buffer = new byte[stream.Length];
+//            stream.Read(buffer, 0, buffer.Length);
+            
+//        }
+
+//        command.Parameters["@title"].Value = file.Value;
+//        command.Parameters["@file_name"].Value = file.Key;
+//        command.Parameters["@binary_data"].Value = buffer;
+
+//        await command.ExecuteNonQueryAsync();
+//    }
+//}
+
+List<DataFile> dataFiles = new List<DataFile>();
+
+string commandString = "SELECT * FROM Files";
+
+using(SqlConnection connection = new SqlConnection(connectionString))
 {
     await connection.OpenAsync();
 
     SqlCommand command = connection.CreateCommand();
     command.CommandText = commandString;
-    command.Parameters.Add("@title", SqlDbType.NVarChar, 100);
-    command.Parameters.Add("@file_name", SqlDbType.NVarChar, 100);
-    command.Parameters.Add("@binary_data", SqlDbType.VarBinary);
 
-    //command.Parameters.Add("@file_name", SqlDbType.VarBinary, 100);
-    byte[] buffer;
-
-    foreach(var file in files)
+    using(SqlDataReader reader = command.ExecuteReader())
     {
-        using(FileStream stream = new(file.Key, FileMode.Open))
+        if(reader.HasRows)
         {
-            buffer = new byte[stream.Length];
-            stream.Read(buffer, 0, buffer.Length);
-            
+            while(await reader.ReadAsync())
+            {
+                int id = reader.GetInt32(0);
+                string title = reader.GetString(1);
+                string fileName = reader.GetString(2);
+                byte[] binaryData = (byte[])reader.GetValue(3);
+
+                DataFile file = new(id, title, fileName, binaryData);
+                dataFiles.Add(file);
+            }
         }
+    }
+}
 
-        command.Parameters["@title"].Value = file.Value;
-        command.Parameters["@file_name"].Value = file.Key;
-        command.Parameters["@binary_data"].Value = buffer;
-
-        await command.ExecuteNonQueryAsync();
+if(dataFiles.Count > 0)
+{
+    foreach(DataFile file in dataFiles)
+    {
+        using(FileStream stream = new FileStream(@"D:\" + file.FileName, FileMode.OpenOrCreate))
+        {
+            stream.Write(file.BinaryData, 0, file.BinaryData.Length);
+        }
     }
 }
